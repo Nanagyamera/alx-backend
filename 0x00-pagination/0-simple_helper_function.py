8b5c8855c8f05c02700a591371bc953b6471d39c
Nanagyamera <nanagyamera3@gmail.com>
2023-07-21 08:46:45 -0700

    Add index_range function for pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -13,12 +13,14 @@ def index_range(page, page_size):
         page_size (int): The number of items per page.
 
     Returns:
-        tuple: A tuple containing the start index and the end index (both 0-indexed)
+        tuple: A tuple containing the start index and the end index
+        (both 0-indexed)
         for the given page.
     """
     if page < 1 or page_size < 1:
-        raise ValueError("Page and page_size must be greater than or equal to 1.")
-    
+        raise ValueError("Page and page_size must be
+                         greater than or equal to 1.")
+
     start_index = (page - 1) * page_size
     end_index = start_index + page_size - 1
 
