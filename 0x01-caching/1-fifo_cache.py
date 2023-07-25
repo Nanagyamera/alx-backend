fe2e51179888f8e1fa4cdf311333dd10f617653f
Nanagyamera <nanagyamera3@gmail.com>
2023-07-25 13:20:17 -0700

    Implement FIFOCache class with FIFO eviction policy

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -5,17 +5,21 @@ Task 1
 
 
 class FIFOCache(BaseCaching):
-    """ FIFOCache inherits from BaseCaching and implements a caching system with FIFO eviction policy
+    """
+    FIFOCache inherits from BaseCaching and implements a caching system
+    with FIFO eviction policy
     """
 
     def __init__(self):
-        """ Initialize the FIFOCache
+        """
+        Initialize the FIFOCache
         """
         super().__init__()
         self.queue = []
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """
+        Add an item in the cache
         """
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
@@ -24,7 +28,8 @@ class FIFOCache(BaseCaching):
             self.queue.append(key)
 
     def evict(self):
-        """ Evict the first item in the cache (FIFO eviction)
+        """
+        Evict the first item in the cache (FIFO eviction)
         """
         if self.queue:
             key_to_evict = self.queue.pop(0)
@@ -32,7 +37,8 @@ class FIFOCache(BaseCaching):
             print("DISCARD:", key_to_evict)
 
     def get(self, key):
-        """ Get an item by key
+        """
+        Get an item by key
         """
         if key is not None:
             return self.cache_data.get(key)
