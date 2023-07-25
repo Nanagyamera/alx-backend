67763324e028cda8c62ef762869d0d10481f9efc
Nanagyamera <nanagyamera3@gmail.com>
2023-07-25 12:50:40 -0700

    Add index_range function for pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -22,6 +22,6 @@ def index_range(page, page_size):
                          " greater than or equal to 1.")
 
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size - 1
+    end_index = start_index + page_size
 
     return start_index, end_index
