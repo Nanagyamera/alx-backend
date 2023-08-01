54ba89b890c75e69986c0cf18aedff6a08a7a558
Nanagyamera <nanagyamera3@gmail.com>
2023-08-01 15:33:22 -0700

    Instantiate Babel object with Config for language and timezone

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+A Basic Flask app.
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+# Config class with LANGUAGES class attribute
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# Instantiate Babel object and set app config
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """
+    This is the home page
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
