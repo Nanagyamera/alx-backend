c41d27eb9cf4d4954045878f102ad4b2dec39740
Nanagyamera <nanagyamera3@gmail.com>
2023-08-29 07:45:21 -0700

    Update displaySchoolValue to use async/await

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,31 @@
+#!/usr/bin/yarn dev
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.error('Redis client not connected to the server:', error.message);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await getAsync(schoolName);
+    console.log(`${schoolName}: ${value}`);
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
